dd7fd7d66a9ec597863887c8abb6bb5b797b5370
Unknown <williamcolin527@gmail.com>
2018-08-13 17:23:05 -0500

    First Commit

---
+++ powergy_reports/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Reportes Powergy',
+    'version': '1.0',
+    'summary': 'Personalizacion de reportes para Powergy',
+    'description': 'Adicion de diversos campos para tener obtener diferentes reportes con informacion',
+    'category': 'Personalizacion',
+    'author': 'William Colin Macedo',
+    'website': 'www.xmarts.com',
+    'depends': ['base','purchase'],
+    'data': ['views/view.xml',
+    		 'reports/report_purchase_order.xml',
+             'reports/report_factura.xml',
+             'reports/report_delivery_split.xml',
+             'reports/report_payment_receipt.xml' ],
+
+
+}
---
+++ powergy_reports/models/powergy_model.py
@@ -0,0 +1,24 @@
+from odoo import api, _, tools, fields, models, exceptions
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+from datetime import datetime
+from . import amount_to_text
+
+
+class ReportPurchase(models.Model):
+	"""docstring for ReportPurchase"""
+	_inherit = 'purchase.order'
+
+	observations= fields.Text(string='Observaciones', size='255')
+	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
+                                 help='Amount of the invoice in letter')
+	@api.one
+	@api.depends('amount_total')
+	def _get_amount_to_text(self):
+		self.amount_to_text = amount_to_text.get_amount_to_text(self, self.amount_total)
+
+
+class ReportFactura(models.Model):
+
+	_inherit = 'account.invoice'
+
+	observations= fields.Text(string='Observaciones', size='255')
