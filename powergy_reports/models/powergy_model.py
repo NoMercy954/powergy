a59cd989946fe898b97c17d532829c962978bd47
Unknown <williamcolin527@gmail.com>
2018-09-01 16:10:53 -0500

    Changes new fields in sales.order and new fields in the diferetns reports

--- powergy_reports/models/powergy_model.py
+++ powergy_reports/models/powergy_model.py
@@ -8,9 +8,12 @@ class ReportPurchase(models.Model):
 	"""docstring for ReportPurchase"""
 	_inherit = 'purchase.order'
 
-	observations= fields.Text(string='Observaciones')
+	observations= fields.Text(string='Observations')
 	amount_to_text = fields.Char(compute='_get_amount_to_text', string='Monto en Texto', readonly=True,
                                  help='Amount of the invoice in letter')
+	type_of_order = fields.Selection([('national','National'),
+							   		('international','International')], help='Choose the type of order', default='national',required=True)
+
 	@api.one
 	@api.depends('amount_total')
 	def _get_amount_to_text(self):
@@ -21,7 +24,7 @@ class ReportFactura(models.Model):
 
 	_inherit = 'account.invoice'
 
-	observations= fields.Text(string='Observaciones')
+	observations= fields.Text(string='Observations')
 
 
 class ReportPago(models.Model):
@@ -29,3 +32,12 @@ class ReportPago(models.Model):
 	_inherit = 'account.payment'
 
 	cambio = fields.Float(string='Tipo Cambio')
+
+class QuotationsFields(models.Model):
+	"""docstring for Quitations"""
+	_inherit = 'sale.order'
+
+	indiv_orders = fields.Text(string='Pedidos Individuales')	
+	name_site = fields.Char(string='Nombre de sitio')
+	no_adrisa = fields.Char(string='No. Adrisa/Grafo')
+	folio_patrimonial = fields.Char(string='Folio de pase de acceso de Seguridad Patrimonial')
