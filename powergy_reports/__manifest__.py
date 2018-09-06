37344d98d1a84a985c4c48bee88d6e16d614182a
Unknown <williamcolin527@gmail.com>
2018-09-06 01:26:21 -0500

    .

--- powergy_reports/__manifest__.py
+++ powergy_reports/__manifest__.py
@@ -6,13 +6,14 @@
     'category': 'Personalizacion',
     'author': 'William Colin Macedo',
     'website': 'www.xmarts.com',
-    'depends': ['base','purchase'],
+    'depends': ['base','purchase','contacts'],
     'data': ['views/view.xml',
     		 'reports/report_purchase_order.xml',
              'reports/report_factura.xml',
              'reports/report_delivery_split.xml',
              'reports/report_payment_receipt.xml',
-             'reports/report_saleorder.xml' ],
+             'reports/report_saleorder.xml',
+             'reports/report_albaran.xml' ],
 
 
 }
